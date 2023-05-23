1c713701c9adb7169d4b4b789508904187192781
Vishwas Gowda <vishwasgowda@Vishwass-MacBook-Air.local>
2023-05-23 23:21:17 +0530

    deployment

--- src/connect.py
+++ src/connect.py
@@ -1,12 +1,9 @@
 import psycopg2
+import streamlit as st
 
 def connect():
     conn = psycopg2.connect(
-    database="tracker", 
-    user='postgres', 
-    password='PgAdmin@21', 
-    host='127.0.0.1', 
-    port= '5432'
+    **st.secrets["postgres"]
     )
     cursor = conn.cursor()
     return cursor
